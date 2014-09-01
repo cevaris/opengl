c948bcc10ae48a66e3476db7346e5e7cc674a91b
Adam Cardenas <cevaris@gmail.com>
2014-09-01 12:59:10 -0600

    first commit

---
+++ opengl/main.cpp
@@ -0,0 +1,11 @@
+//
+//  main.cpp
+//  opengl
+//
+//  Created by Adam Cardenas on 9/1/14.
+//  Copyright (c) 2014 OpenGL - Adam Cardenas. All rights reserved.
+//
+
+#include "main.h"
+
+// -framework OpenGL -framework GLUT -Wno-deprecated
---
+++ opengl/main.h
@@ -0,0 +1,14 @@
+//
+//  main.h
+//  opengl
+//
+//  Created by Adam Cardenas on 9/1/14.
+//  Copyright (c) 2014 OpenGL - Adam Cardenas. All rights reserved.
+//
+
+#ifndef __opengl__main__
+#define __opengl__main__
+
+#include <iostream>
+
+#endif /* defined(__opengl__main__) */
